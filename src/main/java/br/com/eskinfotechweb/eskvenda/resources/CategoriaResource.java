270cc88f40a8a319d229afa0e0612203624f8c3f
Edson Shideki Kokado <eskokado@gmail.com>
2019-07-19 08:20:46 -0300

    Testando a classe de domain - Categoria

--- src/main/java/br/com/eskinfotechweb/eskvenda/resources/CategoriaResource.java
+++ src/main/java/br/com/eskinfotechweb/eskvenda/resources/CategoriaResource.java
@@ -1,15 +1,28 @@
 package br.com.eskinfotechweb.eskvenda.resources;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import br.com.eskinfotechweb.eskvenda.domain.Categoria;
 
 @RestController
 @RequestMapping("/categorias")
 public class CategoriaResource {
 
 	@GetMapping
-	public String findAll() {
-		return "Rest funcionando";
+	public List<Categoria> findAll() {
+		
+		Categoria cat1 = new Categoria(1L, "Informática");
+		Categoria cat2 = new Categoria(2L, "Escritório");
+		
+		List<Categoria> lista = new ArrayList<>();
+		lista.add(cat1);
+		lista.add(cat2);
+		
+		return lista;
 	}
 }
