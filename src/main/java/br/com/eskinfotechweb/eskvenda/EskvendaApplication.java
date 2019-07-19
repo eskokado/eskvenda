6fc85f42aadf644efaa61356dbd4e58d0f6254d2
Edson Shideki Kokado <eskokado@gmail.com>
2019-07-19 08:01:11 -0300

    Projeto criado

---
+++ src/main/java/br/com/eskinfotechweb/eskvenda/EskvendaApplication.java
@@ -0,0 +1,13 @@
+package br.com.eskinfotechweb.eskvenda;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EskvendaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EskvendaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/eskinfotechweb/eskvenda/EskvendaApplicationTests.java
@@ -0,0 +1,16 @@
+package br.com.eskinfotechweb.eskvenda;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class EskvendaApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
